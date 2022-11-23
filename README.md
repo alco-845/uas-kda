5ce1d6a79890bf786d6e27f3bc2c5fd2e1fff286
alco-845 <nicola.alivant@gmail.com>
2022-11-23 20:41:19 +0700

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+## To Do:
+1. Create database kda_jwt
+2. Migrate and seed
+
+## Login url:
+http://127.0.0.1:8000/login
+
+header:
+- username
+- password
+
+## Check is authorized or not
+http://127.0.0.1:8000/user/{id}
+
+authorization:
+bearer {token}
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $user = User::where('username', $username)->first();
+
+        if (Hash::check($password, $user->password)) {
+            $apiToken = base64_encode(Str::random(40));
+
+            $user->update([
+                'api_token' => $apiToken
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login Success',
+                'data' => [
+                    'user' => $user,
+                    'api_token' => $apiToken
+                ]
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed',
+                'data' => ''
+            ]);
+        }
+    }
+}
